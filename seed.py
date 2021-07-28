f2b8ba6b94b612f02784c9f096de765f2e6623a6
Chintak <chintak.soni75@gmail.com>
2021-07-28 16:29:36 +0530

    changes

--- seed.py
+++ seed.py
@@ -5,8 +5,16 @@ class Seeder(object):
     def populate_database(self):
         record = Menu.query.first()
         if not record:
-            new_record = Menu(name="Baked potatoes")
+            new_record = Menu(name="Baked potatoes",price="10",quantity="500gm")
             db.session.add(new_record)
+
+            new_record = Menu(name="Garlic bread",price="50",quantity="2pcs")
+            db.session.add(new_record)
+            new_record = Menu(name="Fried rice",price="100",quantity="500gm")
+            db.session.add(new_record)
+            new_record = Menu(name="Soup",price="30",quantity="500gm")
+            db.session.add(new_record)
+
             db.session.commit()
 
 if __name__ == '__main__':
